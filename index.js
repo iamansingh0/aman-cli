0ee30a54cbdc8f9be53fa61fb24b89003e89d131
iamansingh0 <amankumar7355197337@gmail.com>
2023-06-21 11:54:18 +0530

    commiit

--- index.js
+++ index.js
@@ -7,7 +7,8 @@
  * @author Aman K <https://aman-kumar-singh.vercel.app/>
  */
 
-
+const chalk = require('chalk');
+// const boxen = require('boxen');
 const init = require('./utils/init');
 const cli = require('./utils/cli');
 const log = require('./utils/log');
@@ -18,6 +19,7 @@ const flags = cli.flags;
 const { clear, debug } = flags;
 
 (async () => {
+	const { default: boxen } = await import('boxen');
 	init({ clear });
 	input.includes(`help`) && cli.showHelp(0);
 
@@ -25,14 +27,19 @@ const { clear, debug } = flags;
 
 	if (input[0] === 'joke') {
 		const res = await axios.get('https://api.chucknorris.io/jokes/random');
-		console.log(res.data.value);
+		const joke = res.data.value;
+		const coloredJoke = chalk.yellow(joke);
+		const boxedJoke = boxen(coloredJoke, { padding: 1, margin: 1, borderStyle: 'round', borderColor: 'yellow' });
+		console.log(boxedJoke);
 	}
 	if(input[0] === 'quote') {
 		try {
 			const res = await axios.get('https://api.quotable.io/quotes/random?limit=1')
 			const data = res.data[0].content;
-			const author = res.data[0].authorSlug
-			console.log(`${data} ~ by ${author}`)
+			const author = res.data[0].authorSlug;
+			const coloredQuote = chalk.cyan(`${data} ~ by ${author}`);
+			const boxedQuote = boxen(coloredQuote, { padding: 1, margin: 1, borderStyle: 'double', borderColor: 'yellow' });
+			console.log(boxedQuote);
 		} catch(error) {
 			console.log(error)
 		}
