d9134c87aa4a0a45860f0346df3965289ed422d2
iamansingh0 <amankumar7355197337@gmail.com>
2023-06-25 15:21:06 +0530

    commit

--- utils/weather.js
+++ utils/weather.js
@@ -14,14 +14,14 @@ const farhToCelsius = (data) => {
 	return cels.toFixed(1);
 }
 
-const displayTemps = (data) => {
+const displayTemps = (data, wind) => {
 	const temp = farhToCelsius(data.temp)
 	const feels_like = farhToCelsius(data.feels_like)
 	const temp_min = farhToCelsius(data.temp_min)
 	const temp_max = farhToCelsius(data.temp_max)
 	const humidity = data.humidity
 	console.log(`Current Temp: ${temp} °C | Feels Like: ${feels_like} °C\nMaximum Temp: ${temp_max} °C | Minimum Temp: ${temp_min} °C`)
-	console.log(`Humidity: ${humidity}%`)
+	console.log(`Humidity: ${humidity}%         | Wind Speed: ${wind.speed}mph`)
 }
 
 const displayCountryCity = (countryCode) => {
@@ -65,8 +65,9 @@ const fetchWeather = async (city, api) => {
 		  });
 		  
 		// console.log("\n")
-		displayTemps(weatherData.main);
-		console.log("\n")
+		displayTemps(weatherData.main, weatherData.wind);
+		// console.log("\n
+		console.log('______________________________\n');
 	} catch (e) {
 		console.log(e);
 	}
